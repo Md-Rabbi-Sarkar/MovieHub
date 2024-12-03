49e9036f1b47af51f75b3b45fa5a16932e1fe906
Md-Rabbi-Sarkar <noreply@github.com>
2024-12-03 23:22:02 +0600

    deploy on firebase

---
+++ src/provider/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD63X9NpAhm1GODZB0zwBnhI3zIopURxRc",
+  authDomain: "movie-hub-client.firebaseapp.com",
+  projectId: "movie-hub-client",
+  storageBucket: "movie-hub-client.firebasestorage.app",
+  messagingSenderId: "309896160447",
+  appId: "1:309896160447:web:d6fdd273717fa4d803d20a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
